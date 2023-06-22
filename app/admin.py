a3a008ccda5a41327aebb38d3d13844a51ec9653
DineshMaddamsetty <dinesh.maddamsetty@gmail.com>
2023-06-22 20:48:16 +0530

    first commit

---
+++ app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from app.models import *
+admin.site.register(Topic)
+admin.site.register(WebPage)
+admin.site.register(AccessRecords)
+# Register your models here.
